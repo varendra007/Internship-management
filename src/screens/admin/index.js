130f9de59f969e8b4cf5afd338e0dcf3556d4f87
Varendra Maurya <varendramaurya@gmail.com>
2022-10-27 22:37:24 +0530

    authentication ui

---
+++ src/screens/admin/index.js
@@ -0,0 +1,100 @@
+import { TextField } from '@mui/material';
+import React, { useState } from 'react';
+const classes = {
+	button: {
+		border: 'none',
+		borderRadius: '100px',
+		width: '150px',
+		height: '55px',
+		backgroundColor: '#fa2d64',
+		color: '#ffffff',
+		fontStyle: 'normal',
+		fontFamily: 'Inter',
+		fontWeight: '600',
+		textAlign: 'center',
+		fontSize: '26px',
+		cursor: 'pointer',
+	},
+	fontname: {
+		fontFamily: 'Inter',
+	},
+};
+const AdminScreen = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const handleSubmit = () => {};
+	return (
+		<div
+			style={{
+				height: '100vh',
+				width: '100vw',
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}
+		>
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'space-evenly',
+					alignItems: 'center',
+					flexDirection: 'column',
+					width: '500px',
+					boxShadow: '0px 0px 15px -1px rgba(0,0,0,0.74)',
+					borderRadius: '12px',
+				}}
+			>
+				<h1
+					style={{
+						fontSize: '2em',
+						fontWeight: 'bold',
+						...classes.fontname,
+					}}
+				>
+					Login
+				</h1>
+				<form
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						width: '100%',
+						...classes.fontname,
+					}}
+					onSubmit={handleSubmit}
+				>
+					<TextField
+						id="outlined-basic"
+						label="Email"
+						name="email"
+						variant="outlined"
+						type="email"
+						required
+						value={email}
+						onChange={(evt) => setEmail(evt.target.value)}
+						style={{ width: '80%', ...classes.fontname }}
+					/>
+					<br />
+					<TextField
+						id="outlined-basic"
+						label="Password"
+						name="email"
+						variant="outlined"
+						type="password"
+						required
+						value={password}
+						onChange={(evt) => setPassword(evt.target.value)}
+						style={{ width: '80%', ...classes.fontname }}
+					/>
+					<br />
+					<button style={classes.button} className="defaultButtonHover1">
+						Login
+					</button>
+					<br />
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default AdminScreen;
