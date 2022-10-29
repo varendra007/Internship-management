39f5be8f213c2d04b0f48d951112f1c6e5f2d16e
Varendra Maurya <varendramaurya@gmail.com>
2022-10-30 02:26:36 +0530

    all mentors done

--- src/screens/admin/AllMentorsWithInterns.js
+++ src/screens/admin/AllMentorsWithInterns.js
@@ -30,46 +30,50 @@ const AllMentorsWithStudents = () => {
 	}, []);
 	return (
 		<div style={{ width: '100vw', overflow: 'hidden' }}>
-			<div
-				id="tableLandlord"
-				className="w-full overflow-auto"
-				style={{ width: '100vw', overflow: 'auto' }}
-			>
-				<h2>List of all mentors with their interns</h2>
-				{users.length > 0 && (
-					<table
-						className="overflow-scroll"
-						style={{ width: '100%', textAlign: 'center' }}
-					>
-						<thead>
-							<tr>
-								<th className="thx">Mentor Email</th>
-								<th className="thx">Mentor Name </th>
-								<th className="thx">Intern Email</th>
-								<th className="thx">Intern Name</th>
-								<th className="thx">Intern Contact</th>
-							</tr>
-						</thead>
-						<tbody>
-							{users.map((el, ind) => {
-								return (
-									<tr key={ind}>
-										<td className="tdx">{el.mentor.email_id}</td>
-										<td className="tdx">
-											{el.mentor.first_name} {el.mentor.last_name}
-										</td>
-										<td className="tdx">{el.intern.email_id}</td>
-										<td className="tdx">
-											{el.intern.first_name} {el.intern.last_name}
-										</td>
-										<td className="tdx">{el.intern.phone}</td>
-									</tr>
-								);
-							})}
-						</tbody>
-					</table>
-				)}
-			</div>
+			{users.length > 0 ? (
+				<div
+					id="tableLandlord"
+					className="w-full overflow-auto"
+					style={{ width: '100vw', overflow: 'auto' }}
+				>
+					<h2>List of all mentors with their interns</h2>
+					{users.length > 0 && (
+						<table
+							className="overflow-scroll"
+							style={{ width: '100%', textAlign: 'center' }}
+						>
+							<thead>
+								<tr>
+									<th className="thx">Mentor Email</th>
+									<th className="thx">Mentor Name </th>
+									<th className="thx">Intern Email</th>
+									<th className="thx">Intern Name</th>
+									<th className="thx">Intern Contact</th>
+								</tr>
+							</thead>
+							<tbody>
+								{users.map((el, ind) => {
+									return (
+										<tr key={ind}>
+											<td className="tdx">{el.mentor.email_id}</td>
+											<td className="tdx">
+												{el.mentor.first_name} {el.mentor.last_name}
+											</td>
+											<td className="tdx">{el.intern.email_id}</td>
+											<td className="tdx">
+												{el.intern.first_name} {el.intern.last_name}
+											</td>
+											<td className="tdx">{el.intern.phone}</td>
+										</tr>
+									);
+								})}
+							</tbody>
+						</table>
+					)}
+				</div>
+			) : (
+				<div>No Interns found please Hire New!</div>
+			)}
 		</div>
 	);
 };
